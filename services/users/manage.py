f3fea80951337136158d67291da383d273292f35
Forest Altherr <faltherr@pn1706924.lanl.gov>
2018-11-26 17:52:48 -0700

    Postgres container in process

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,9 +3,17 @@
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
 
 cli = FlaskGroup(app)
+
+# This registers a new command, recreate_db, to the CLI so that we can run it from the command line, which we’ll use shortly to apply the model to the database.
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
